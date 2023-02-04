c7a9fde3a68f5b2bbd6cf60a31af865fa52520e2
Zak B. Elep <zakame@zakame.net>
2023-02-04 07:36:58 +0000

    Initial commit
    Completely done using GitHub Codespaces from the Data-Cuid repository.

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Data::Cuid2 - It's new $module
+
+# SYNOPSIS
+
+    use Data::Cuid2;
+
+# DESCRIPTION
+
+Data::Cuid2 is ...
+
+# LICENSE
+
+Copyright (C) Zak B. Elep.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Zak B. Elep <zakame@zakame.net>
---
+++ lib/Data/Cuid2.pm
@@ -0,0 +1,39 @@
+package Data::Cuid2;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Cuid2 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Cuid2;
+
+=head1 DESCRIPTION
+
+Data::Cuid2 is ...
+
+=head1 LICENSE
+
+Copyright (C) Zak B. Elep.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Zak B. Elep E<lt>zakame@zakame.netE<gt>
+
+=cut
+
